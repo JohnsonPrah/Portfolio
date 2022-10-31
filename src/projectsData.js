446e6b7cb170111e14cbeac9f11056bafae632c6
owusu prah johnson <owusuprahjohnson@yahoo.com>
2022-10-31 01:03:52 -0700

    first commit

--- src/projectsData.js
+++ src/projectsData.js
@@ -1,20 +1,13 @@
 const data=[
     {
-        id : "1",
-        display : "boreal.png",
-        title : "Boreal Coffee Clone",
-        description : "I decided to recreate the Frontend of Boreal Coffee's official Web App as I got attracted to their beautiful UI and it was a great opportunity for me to showcase my frontend skills by rebuilding the UI and it ended up being a great experience for me I used my frontend tools to build the User Interface which works on all types of devices along with the SPA library to make the frontend perform faster,using React and Tailwindcss",
-        github : "https://github.com/JohnsonPrah/Boreal-Coffee-Clone",
-        live : "https://boreal-clone.netlify.app/"
+        id : "4",
+        display : "estate.png",
+        title : "Wildermere Real Estate",
+        description : "This is Real Estate react app displaying details about homes and apartments both for sale and rent in the UAE,based on the individual's seacrh input. Technology used includes Next.js,tailwindcss",
+        github : "https://github.com/JohnsonPrah/real-estate",
+        live : "https://wildermere-estates.netlify.app/"
     },
-    {
-        id : "2",
-        display : "Coffee.png",
-        title : "Nostalgia Farm clone",
-        description : "I re-created the frontend of Boreal Coffee's official web app because I got attracted to their beautiful UI. It was a great experience for me to build the entire frontend. ",
-        github : "https://github.com/JohnsonPrah/Nostalgia-Website-Clone",
-        live : "https://nostalgia-clone.netlify.app/"
-    },
+
     {
         id : "3",
         display : "gaming.PNG",
@@ -23,13 +16,23 @@ const data=[
         github : "https://github.com/JohnsonPrah/game-scope",
         live : "https://game-scope.netlify.app/"
     },
+
     {
-        id : "4",
-        display : "estate.png",
-        title : "Wildermere Real Estate",
-        description : "This is Real Estate react app displaying details about homes and apartments both for sale and rent in the UAE,based on the individual's seacrh input. Technology used includes Next.js,tailwindcss",
-        github : "https://github.com/JohnsonPrah/real-estate",
-        live : "https://wildermere-estates.netlify.app/"
+        id : "2",
+        display : "Coffee.png",
+        title : "Nostalgia Farm clone",
+        description : "I re-created the frontend of Boreal Coffee's official web app because I got attracted to their beautiful UI. It was a great experience for me to build the entire frontend. ",
+        github : "https://github.com/JohnsonPrah/Nostalgia-Website-Clone",
+        live : "https://nostalgia-clone.netlify.app/"
+    },
+
+    {
+        id : "1",
+        display : "boreal.png",
+        title : "Boreal Coffee Clone",
+        description : "I decided to recreate the Frontend of Boreal Coffee's official Web App as I got attracted to their beautiful UI and it was a great opportunity for me to showcase my frontend skills by rebuilding the UI and it ended up being a great experience for me I used my frontend tools to build the User Interface which works on all types of devices along with the SPA library to make the frontend perform faster,using React and Tailwindcss",
+        github : "https://github.com/JohnsonPrah/Boreal-Coffee-Clone",
+        live : "https://boreal-clone.netlify.app/"
     }
 
 ]
